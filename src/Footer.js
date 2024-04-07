a27de49e42331ed214d0ead04316236f45b129ab
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-04-07 21:24:28 +0200

    completed first react-project

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = ({length}) => {
+  return (
+    <footer>
+      <p>You have {length} {length === 1 ? "item" : "items"}</p>
+    </footer>
+  )
+}
+
+export default Footer
