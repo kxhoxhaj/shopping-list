7b834185da47172b85ce88637d9b0d76fe5c686e
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-04-09 21:03:40 +0200

    -madded useEffect to save list items upon changing items state

--- src/App.js
+++ src/App.js
@@ -3,38 +3,38 @@ import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from "react"
+import { useState , useEffect } from "react"
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('shoppinglist'))
-    /* loads the list from local storage */
-  );
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
 
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('shoppinglist', JSON.stringify(items));
+  },[items])
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
     /* saves the list to local storage */
   }
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id +1 : 1;
     const myNewItem = {id: id, checked: false, item}
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ?
     { ...item, checked: !item.checked } : item);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleSubmit = (e) => {
